4d2404852217bbe2ff7c63af6715567f202eed87
rA9k <rohanchauhan9769@gmail.com>
2024-04-03 00:28:27 +0530

    updated script.js

--- script.js
+++ script.js
@@ -3,24 +3,10 @@ document.getElementById('location-form').addEventListener('submit', getWeather);
 async function getWeather(e) {
   e.preventDefault(); 
 
-  let apiKey = "ff2946746dc2a2cc49200f6661eecd35";  // Your OpenWeatherMap API Key
+  let apiKey = "ENTER_YOUR_API_KEY_HERE";  // Your OpenWeatherMap API Key
   let location = document.querySelector("#location-input").value;
   let API = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
 
-  // fetch(API)
-  //   .then(response => {
-  //     if (!response.ok) {
-  //       // Check for 404 specifically
-  //       if (response.status === 404) {
-  //         throw new Error("City not found"); 
-  //       } else {
-  //         throw new Error(`HTTP error: ${response.status}`); 
-  //       }
-  //     }
-  //     return response.json();
-  //   })
-  //   .then(data => displayWeather(data))
-  //   .catch(error => displayError(error));
   try{
     let res = await fetch(API);
     if(!res.ok){
